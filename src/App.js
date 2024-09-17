7cf2d802eb70f4fcf36f006c35f4a3147294be7a
Akhmedov Botir <134036244+AkhmedovBotir@users.noreply.github.com>
2024-09-17 23:35:56 +0500

    new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { SidebarMenu } from './components';
+
+function App() {
+  return (
+    <>
+      <SidebarMenu />
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import '@coreui/coreui/dist/css/coreui.min.css'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
